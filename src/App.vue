fb09650ddd158c9f533c0eaab6d799d0d6f9a31a
xiaofei4real <83374452@qq.com>
2023-08-02 07:17:51 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <h1 v-color>test1</h1>
+    <!-- <h1 v-colorred="colorred">TEST2</h1> -->
+    <h2 v-color="'green'">test3</h2>
+    <button @click="color='blue'">change color</button>
+    <hr>
+
+    <div>
+      <Left></Left>
+    </div>
+  </div>
+</template>
+
+<script>
+import Left from '@/components/Left.vue'
+
+export default {
+  data() {
+    return {
+      color:'red'
+    }
+  },
+  directives: {
+    color: {
+      bind(el,binding) {
+        console.log('touch off')
+        console.log(el,binding)
+        el.style.color = binding.value
+      },
+      update(el,binding) {
+        el.style.color=binding.value
+      }
+    }
+  },
+  methods: {
+    // change() {
+    //   this.style.color='biue'
+    //   console.log(this)
+    // }
+  },
+  components: {
+    Left
+  }
+ }
+</script>
+
+<style >
+  /* .container{
+    
+   
+  } */
+</style>
+
+1123124
---
+++ src/components/Left.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="left">
+    <h1>left</h1>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<style>
+  .left {
+    width: 200px;
+    height: 200px;
+    background-color: aquamarine;
+   
+  }
+</style>
